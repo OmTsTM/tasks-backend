92e9c78277b15179ffca391bc669cc12434bf9fc
MATTEUS TEKESHI DIAS <matteus_mindlabs@getnet.com.br>
2024-03-04 02:17:06 -0300

    adição de e-mail de erro

--- src/test/java/br/ce/wcaquino/taskbackend/utils/DataUtilsTest.java
+++ src/test/java/br/ce/wcaquino/taskbackend/utils/DataUtilsTest.java
@@ -9,7 +9,7 @@ public class DataUtilsTest {
     @Test
     public void deveRetornarTrueParaDatasFuturas() {
         LocalDate date = LocalDate.of(2030, 01, 01);
-        Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));
+        Assert.assertFalse(DateUtils.isEqualOrFutureDate(date));
     }
 
     @Test
